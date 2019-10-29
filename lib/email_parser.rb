e5342b2f013cb7bdb00e8670d77bc9a452fc2501
Michael Carroll <carrollm2@gmail.com>
2019-10-29 12:39:35 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,6 +12,7 @@ class EmailAddressParser
   end
 
   def parse
+    email_address_array = email_addresses.delete!(" ")
     email_address_array = email_addresses.split(",")
     #email_address_array = email_addresses.delete!(" ").split(",")
     parsed_emails = email_address_array.uniq
