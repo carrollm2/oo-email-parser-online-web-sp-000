39dde22e26ab8bd6745fa343ab09dcff1172a373
Michael Carroll <carrollm2@gmail.com>
2019-10-29 12:45:54 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -20,6 +20,7 @@ class EmailAddressParser
     email_address_array.each do |email|
       if email != '""'
         parsed_emails.push(email)
+      end
     end
 
     parsed_emails = email_address_array.uniq
